7a22601a6a1624c74e2d5a7e1346d51a1d27519e
Mehdiya-Naqvi <mehdiya313naqvi@gmail.com>
2024-09-23 16:48:48 +0530

    Update password.py

--- Password-Generator/password.py
+++ Password-Generator/password.py
@@ -1,5 +1,5 @@
 import random
-chars = "abcdefghijklmnopqrstuvwxyz !@#$%&*() ABCDEFGHIJLKMNOPQRSTUVWXYZ 0123456789"
+chars = "abcdefghijklmnopqrstuvwxyz!@#$%&*()ABCDEFGHIJLKMNOPQRSTUVWXYZ0123456789"
 len = int(input("Enter the length of your Password :- "))
 password = ""
 for i in range(len):
